440e806670020e65d256045414d60ea84b435c51
Veah <Veah@qq.com>
2019-07-13 23:49:49 +0800

    注释完成

--- js/commond.js
+++ js/commond.js
@@ -1,11 +1,10 @@
-let PYG = {};
-
-PYG.littleCar = function () {
-    let dataString = localStorage.getItem('myshopcar');
+let PYG = {};               //防止函数污染
+PYG.littleCar = function () {               //防止函数污染
+    let dataString = localStorage.getItem('myshopcar');     //获得本地数据库
     let shopArr = JSON.parse(dataString) || [];
     let number = 0;
-    shopArr.forEach(function (e, i) {
-        number += e.number;
+    shopArr.forEach(function (e) {
+        number += e.number;                             //遍历数组,将其中元素的number属性累加
     })
-    $('.shopcar .count').text(number);
+    $('.shopcar .count').text(number);                              //修改小红点
 }
